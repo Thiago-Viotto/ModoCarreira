ef7464ef8e16444a9f81c778ba3f9cabb7aa155b
Thiago Henrique Viotto <thiagohenriqueviotto@gmail.com>
2021-03-02 00:00:40 -0300

    added navBar #3

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,8 +1,38 @@
 import './Nav.css';
-import React from 'react';
+import React, { useEffect } from 'react';
+import $ from "jquery";
 
-export default props =>
-    <aside className="menu-area">
-        Navegação
-    </aside>
-  
+export default props => {
+
+    useEffect(() => {
+        $(document).ready(function () {
+            $('.sidenav').sidenav();
+        });
+    })
+
+    return (
+        <>
+            <nav>
+                <div className="nav-wrapper">
+                    <ul class="left hide-on-med-and-down">
+                        <li><a href="sass.html">Início</a></li>
+                        <li><a href="sass.html">História de Morais</a></li>
+                    </ul>
+                    <a href="#!" class="brand-logo center">Logo</a>
+                    <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                    <ul class="right hide-on-med-and-down">
+                        <li><a href="sass.html">Criar notícias</a></li>
+                        <li><a href="sass.html">Criar histórico</a></li>
+                    </ul>
+                </div>
+            </nav>
+
+            <ul class="sidenav" id="mobile-demo">
+                <li><a href="sass.html">Início</a></li>
+                <li><a href="sass.html">História de Morais</a></li>
+                <li><a href="sass.html">Criar notícias</a></li>
+                <li><a href="sass.html">Criar histórico</a></li>
+            </ul>
+        </>
+    )
+}
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,13 +1,13 @@
 import './App.css';
+import '../css/materialize.min.css';
 import React from 'react';
 import Main from '../components/template/Main';
-import Logo from '../components/template/Logo';
 import Nav from '../components/template/Nav';
 import Footer from '../components/template/Footer';
 
+
 export default props =>
     <div className="app">
-        <Logo />
         <Nav />
         <Main />
         <Footer />
