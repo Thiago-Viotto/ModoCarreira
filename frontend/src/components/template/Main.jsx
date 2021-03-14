744fa9fb53387a645253c7ed83972f9e24dd3fd3
Thiago Henrique Viotto <thiagohenriqueviotto@gmail.com>
2021-03-14 14:04:32 -0300

    added card #6

--- frontend/src/components/template/Main.jsx
+++ frontend/src/components/template/Main.jsx
@@ -1,11 +1,35 @@
 import './Main.css';
 import React from 'react';
-import Header from './Header';
 
-export default props =>
-    <>
-        <Header/>
-        <main className="content">
-            Conteúdo
-        </main>
-    </>
+export default props => {
+
+    return (
+        <>
+            <main className="content">
+
+
+                <div class="row">
+                    <div class="col s17 m4">
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="images/teste.png" alt="Imagem da postagem" />
+                                <span class="card-title">Card Title</span>
+                            </div>
+                            <div class="card-content">
+                                <p>I am a very simple card. I am good at containing small bits of information.
+                                I am convenient because I require little markup to use effectively.</p>
+                            </div>
+                            <div class="card-action">
+                                <a href="#">This is a link</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                
+            </main>
+        </>
+    )
+}
+
