8b9acd60221cd27cf260865c5a3f17f94ad03caa
Thiago Henrique Viotto <thiagohenriqueviotto@gmail.com>
2021-03-14 14:29:57 -0300

    set responsive card #6

--- frontend/src/components/template/Main.jsx
+++ frontend/src/components/template/Main.jsx
@@ -7,9 +7,8 @@ export default props => {
         <>
             <main className="content">
 
-
                 <div class="row">
-                    <div class="col s17 m4">
+                    <div class="col s6 m4">
                         <div class="card">
                             <div class="card-image">
                                 <img src="images/teste.png" alt="Imagem da postagem" />
@@ -17,17 +16,47 @@ export default props => {
                             </div>
                             <div class="card-content">
                                 <p>I am a very simple card. I am good at containing small bits of information.
-                                I am convenient because I require little markup to use effectively.</p>
-                            </div>
-                            <div class="card-action">
-                                <a href="#">This is a link</a>
+                                I am convenient because I require little markup to use effectively.
+                                I am a very simple card. I am good at containing small bits of information.
+                                I am convenient because I require little markup to use effectively</p>
                             </div>
                         </div>
                     </div>
+
+
+                    <div class="col s6 m4">
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="images/teste.png" alt="Imagem da postagem" />
+                                <span class="card-title">Card Title</span>
+                            </div>
+                            <div class="card-content">
+                                <p>I am a very simple card. I am good at containing small bits of information.
+                                I am convenient because I require little markup to use effectively.
+                                I am a very simple card. I am good at containing small bits of information.
+                                I am convenient because I require little markup to use effectively</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="col s6 m4">
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="images/teste.png" alt="Imagem da postagem" />
+                                <span class="card-title">Card Title</span>
+                            </div>
+                            <div class="card-content">
+                                <p>I am a very simple card. I am good at containing small bits of information.
+                                I am convenient because I require little markup to use effectively.
+                                I am a very simple card. I am good at containing small bits of information.
+                                I am convenient because I require little markup to use effectively</p>
+                            </div>
+                        </div>
+                    </div>
+
+
                 </div>
-
-
-                
             </main>
         </>
     )
