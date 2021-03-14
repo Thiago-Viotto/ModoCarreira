af9bd46d18ed8a2b9fe10d0b3694b05bc9a60433
Thiago Henrique Viotto <thiagohenriqueviotto@gmail.com>
2021-03-14 14:49:44 -0300

    changed main card #7

--- frontend/src/components/template/Main.jsx
+++ frontend/src/components/template/Main.jsx
@@ -9,7 +9,7 @@ export default props => {
 
                 <div class="row">
 
-                    <div class="col s12 m4">
+                    <div class="col s12 m7">
                         <div class="card">
                             <div class="card-image">
                                 <img src="images/teste.png" alt="Imagem da postagem" />
@@ -20,10 +20,14 @@ export default props => {
 
 
                     <div class="col s12 m4">
-                        <div class="card">
+                        <div class="card medium">
                             <div class="card-image">
                                 <img src="images/teste.png" alt="Imagem da postagem" />
                                 <span class="card-title">Card Title</span>
+                            </div>
+                            <div class="card-content">
+                                <p>I am a very simple card. I am good at containing small bits of information.
+                                I am convenient because I require little markup to use effectively. </p>
                             </div>
                         </div>
                     </div>
