8e53607b7de3df3c464cfb9678343acea87a5bac
Thiago Henrique Viotto <thiagohenriqueviotto1@gmail.com>
2021-04-18 20:05:00 -0300

    #13

--- frontend/src/components/template/FormMain.jsx
+++ frontend/src/components/template/FormMain.jsx
@@ -1,36 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FormMain.css';
+const axios = require('axios');
 
 export default props => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [valueURLImg, setValueURLImg] = useState('');
+
+    const createNew = async () => {
+        const data = new FormData()
+        data.append('file', selectedFile);
+        console.log(selectedFile)
+
+        axios.post("http://localhost:8000/upload", data, {
+        }).then(res => { })
+
+        if (selectedFile.name !== null) {
+            setValueURLImg('http://localhost:3000/images/' + selectedFile.name)
+        }
+
+        await axios.post('http://localhost:3001/news', {
+            title: title,
+            description: description,
+            image: valueURLImg
+        }).then(resp => {
+            console.log(resp.data)
+        }).catch(error => {
+            console.log(error)
+        })
+    }
 
     return (
         <div class="row">
             <div><h3 id="titleFormMain">Criar notícias</h3></div>
 
-            <form class="col s12" action="#">
+            <form class="col s12" action="#" encType="multipart/form-data">
                 <div class="row">
                     <div class="input-field col s12">
                         <i class="material-icons prefix">subtitles</i>
-                        <input id="icon_prefix" type="text" class="validate"></input>
+                        <input id="icon_prefix" type="text" class="validate" value={title} onChange={e => setTitle(e.target.value)} ></input>
                         <label for="icon_prefix">Título da notícia</label>
                     </div>
                     <div class="input-field col s12">
                         <i class="material-icons prefix">assignment</i>
-                        <textarea id="textarea1" class="materialize-textarea"></textarea>
+                        <textarea id="textarea1" class="materialize-textarea" value={description} onChange={e => setDescription(e.target.value)}></textarea>
                         <label for="textarea1">Descrição da notícia</label>
                     </div>
                 </div>
                 <div class="file-field input-field col s12">
                     <div class="btn" id="btnImage">
                         <span>IMAGEM</span>
-                        <input type="file" />
+                        <input type="file" onChange={(e) => setSelectedFile(e.target.files[0])} />
                     </div>
                     <div class="file-path-wrapper">
                         <input class="file-path validate" type="text" />
                     </div>
                 </div>
                 <div class="col s12" id="btnFormMain">
-                    <button class="btn waves-effect waves-light" type="submit" name="action">CRIAR
+                    <button class="btn waves-effect waves-light" type="submit" name="action" onClick={createNew}>CRIAR
                     <i class="material-icons right">send</i>
                     </button>
                 </div>
